062c902cce81599e700236439a29e460dd24b023
JuanJoseVillamizar <juanjvs1419@gmail.com>
2025-05-03 12:06:52 -0500

    refactor(entity): rename table names from plural to singular
    - Changed @Table annotations for consistency with DB naming

--- Backend/src/main/java/JuanJose/PruebaSomic/entities/Client.java
+++ Backend/src/main/java/JuanJose/PruebaSomic/entities/Client.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Table(name="clients")
+@Table(name="client")
 @Getter
 @Builder
 @NoArgsConstructor
--- Backend/src/main/java/JuanJose/PruebaSomic/entities/Product.java
+++ Backend/src/main/java/JuanJose/PruebaSomic/entities/Product.java
@@ -7,7 +7,7 @@ import java.math.BigDecimal;
 
 
 @Entity
-@Table(name="products")
+@Table(name="product")
 @Getter
 @Builder
 @AllArgsConstructor
